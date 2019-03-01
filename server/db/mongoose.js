3e3d681abb18f8fceb1ef30071228d4308640d30
Hamad Adel <hamadwebdeveloper@gmail.com>
2019-03-02 01:30:55 +0200

    remove process.env.MONGODB_URI from connect function

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+// process.env.MONGODB_URI || 'mongodb+srv://hamad-adel-27017:hamad-atlas-93@developmentcluster-88s7d.mongodb.net/TodoApp
 
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://hamad-adel-27017:hamad-atlas-93@developmentcluster-88s7d.mongodb.net/TodoApp', {useNewUrlParser: true}, (error, client) => {
+mongoose.connect('mongodb+srv://hamad-adel-27017:hamad-atlas-93@developmentcluster-88s7d.mongodb.net/TodoApp', (error, client) => {
     if (error)
       return console.log('Unable to connect to mongodb', error);
     console.log('Connection done');
